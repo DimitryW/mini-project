447a564753412c94a539a4fc8253ea2632173217
DimitryW <grayfen@gmail.com>
2025-04-11 15:10:56 +0800

    revise News model

--- news_crawler/models.py
+++ news_crawler/models.py
@@ -4,9 +4,11 @@ from django.urls import reverse
 
 class News(models.Model):
     title = models.CharField(max_length=255)
+    paragraph = models.TextField()
     content = models.TextField()
     source = models.CharField(max_length=255, blank=True)
-    url = models.URLField(unique=True)
+    news_url = models.URLField(unique=True)
+    image_url = models.URLField(unique=True)
     image = models.ImageField(upload_to="news_images/", blank=True, null=True)
     published_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
